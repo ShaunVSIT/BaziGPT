55372c6bd9924653367e0d1e77ef77aecda1a153
ShaunV <2102172@sit.singaporetech.edu.sg>
2025-07-21 22:55:40 +0700

    Rename neondb.ts to bazidb.ts to fix Vercel case-sensitivity

--- api/famous/index.ts
+++ api/famous/index.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import sql from '../../src/services/neondb';
+import sql from '../../src/services/bazidb';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     try {
